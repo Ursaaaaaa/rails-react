849147b21c26660d6cc054c5e4fa0a2c72398744
Nii Amanor Djoleto <djoletoamanor@gmail.com>
2022-09-30 19:56:12 +0000

    Add react dependencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(greeting: 'Hi')
+Greeting.create(greeting: 'Bonjour')
+Greeting.create(greeting: 'Habari')
+Greeting.create(greeting: 'Ohayo')
+Greeting.create(greeting: 'Konichiwa')
