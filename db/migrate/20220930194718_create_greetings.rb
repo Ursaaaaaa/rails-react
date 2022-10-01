289d418167aa6cabfa4ea8ee97d551666186f391
Nii Amanor Djoleto <djoletoamanor@gmail.com>
2022-10-01 01:33:03 +0000

    Add migration

--- db/migrate/20220930194718_create_greetings.rb
+++ db/migrate/20220930194718_create_greetings.rb
@@ -5,5 +5,10 @@ class CreateGreetings < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    Greeting.create(greeting: 'Ohayo')
+    Greeting.create(greeting: 'Hello')
+    Greeting.create(greeting: 'Good Day')
+    Greeting.create(greeting: 'Sayonara')
+    Greeting.create(greeting: 'Bye')
   end
 end
